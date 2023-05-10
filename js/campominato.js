5cf96be9279f1c2a8d9f0db4ed78f4e9e0f5ecf8
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-10 13:07:55 +0200

    aggiunto commenti

--- js/campominato.js
+++ js/campominato.js
@@ -72,5 +72,25 @@ function createContainerSquare(prendiElement, prendiClass){
 
 
  
-  
+/*
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+        /il computer deve generare 16 numeri compresi da 1 a 100
+
+
+Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+
+In seguito l’utente clicca su una cella: 
+    se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+    Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
+
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
+
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
+*/
  
